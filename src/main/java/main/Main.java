369dbb091c06a8410c3210d0f79040955f137d46
CCaim <barreirocarlos2001@gmail.com>
2023-03-22 11:07:05 +0100

    Añadido Swagger al proyecto

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,38 +2,34 @@ package main;
 
 import java.util.ArrayList;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import main.crud.CartaRepo;
 import main.crud.GenericDAO;
 import main.model.Carta;
 import main.model.Deck;
 import main.model.Usuario;
-@SpringBootApplication
-public class Main {
-	public static <T> void main(String[] args) {
-		
-//		Deck deck1 = new Deck();
-//		
-//		GenericDAO<Deck> genericDAODeck = new GenericDAO(Deck.class);
-//		GenericDAO<Carta> genericDAOCarta = new GenericDAO(Carta.class);
-//		deck1.setNombre("erequetenue2");
-//		Carta carta = genericDAOCarta.buscarPorId(6);
-//		deck1.getCartas().add(carta);
-//		
-//		
-//		
-//		genericDAODeck.insertarRegistroJPA(deck1);
-//	
-//		ArrayList<Deck> user = genericDAODeck.listarRegistros("Deck");
 
-		//main(args);
+
+
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer{
+	
+	public static <T> void main(String[] args) {
+	
+		
 		SpringApplication.run(Main.class, args);
 		
 		
 	}
-	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Main.class);
+	}
 }
 
